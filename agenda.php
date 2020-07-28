908613c9897aa191e6bfb369cdc034449ef382d7
Ktatau <felipeborgescorrea787@gmail.com>
2020-07-28 17:38:41 -0300

    agenda atualizado p1

--- agenda.php
+++ agenda.php
@@ -1,64 +1,111 @@
 <?php
 session_start();
+include_once("conexao_cad.php");
+
+$pega = $_SESSION['usuario'];
+$result_events = "SELECT id, title, color, start, end FROM " .$pega;
+$resultado = mysqli_query($conexao_cad, $result_events);
 ?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset='utf-8' />
-        <link href='css/core/main.min.css' rel='stylesheet' />
-        <link href='css/daygrid/main.min.css' rel='stylesheet' />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/personalizado.css">
+<head>
+<meta charset='utf-8' />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link href='css/main.min.css' rel='stylesheet' />
+<link href='css/agenda.css' rel='stylesheet' />
+<script src='js/main.min.js'></script>
+<script src='js/pt-br.js'></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script>
 
-        <script src='js/core/main.min.js'></script>
-        <script src='js/interaction/main.min.js'></script>
-        <script src='js/daygrid/main.min.js'></script>
-        <script src='js/core/locales/pt-br.js'></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <script src="js/personalizado.js"></script>
-    </head>
-    <body>
-        <?php
-        if (isset($_SESSION['msg'])) {
-            echo $_SESSION['msg'];
-            unset($_SESSION['msg']);
+  document.addEventListener('DOMContentLoaded', function() {
+
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      locale: 'pt-br', 
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+
+      initialDate: new Date(),
+      navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectMirror: true,
+      // select: function(arg) {
+      //   var title = prompt('vc deseja add algo?');
+      //   if (title) {
+      //     calendar.addEvent({
+      //       title: title,
+      //       start: arg.start,
+      //       end: arg.end,
+      //       allDay: arg.allDay
+      //     })
+      //   }
+      //   calendar.unselect()
+     
+          
+      // },
+      select: function (info) {
+            //alert('Início do evento: ' + info.start.toLocaleString());
+            // $('#cadastrar #start').val(info.start.toLocaleString());
+            // $('#cadastrar #end').val(info.end.toLocaleString());
+            // $('#cadastrar').modal('show');
+            $('#cadastrar').dataToggle('modal')
+            $('#cadastrar').dataTarget('myModal')
+        },
+      eventClick: function(arg) {
+        if (confirm('Are you sure you want to delete this event?')) {
+          arg.event.remove()
         }
+      },
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      events: [
+        <?php
+          while($row = mysqli_fetch_array($resultado)){
+            ?>
+          {
+            id: '<?php echo $row['id']; ?>',
+            title: '<?php echo $row['title']; ?>',
+            color: '<?php echo $row['color']; ?>',
+            start: '<?php echo $row['start']; ?>',
+            end: '<?php echo $row['end']; ?>',
+          },
+            <?php
+          }    
         ?>
-        <div id='calendar'></div>
 
-        <div class="modal fade" id="visualizar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Detalhes do Evento</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="visevent">
-                            <dl class="row">
-                                <dt class="col-sm-3">ID do evento</dt>
-                                <dd class="col-sm-9" id="id"></dd>
+      ]
+    });
 
-                                <dt class="col-sm-3">Título do evento</dt>
-                                <dd class="col-sm-9" id="title"></dd>
+    calendar.render();
+  });
 
-                                <dt class="col-sm-3">Início do evento</dt>
-                                <dd class="col-sm-9" id="start"></dd>
+</script>
+        
+</head>
+<body>
 
-                                <dt class="col-sm-3">Fim do evento</dt>
-                                <dd class="col-sm-9" id="end"></dd>
-                            </dl>
-                            <button class="btn btn-warning btn-canc-vis">Editar</button>
-                            <a href="" id="apagar_evento" class="btn btn-danger">Apagar</a>
+  <div id='calendar'></div>
+
+            <div class="modal fade" id="cadastrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Cadastrar Evento</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
-                        <div class="formedit">
-                            <span id="msg-edit"></span>
-                            <form id="editevent" method="POST" enctype="multipart/form-data">
-                                <input type="hidden" name="id" id="id" >
+                        <div class="modal-body">
+                            <span id="msg-cad"></span>
+                            <form id="addevent" method="POST" enctype="multipart/form-data">
                                 <div class="form-group row">
                                     <label class="col-sm-2 col-form-label">Título</label>
                                     <div class="col-sm-10">
@@ -98,75 +145,15 @@ session_start();
 
                                 <div class="form-group row">
                                     <div class="col-sm-10">
-                                        <button type="button" class="btn btn-primary btn-canc-edit">Cancelar</button>
-                                        <button type="submit" name="CadEvent" id="CadEvent" value="CadEvent" class="btn btn-warning">Salvar</button>                                    
+                                        <button type="submit" name="CadEvent" id="CadEvent" value="CadEvent" class="btn btn-success">Cadastrar</button>                                    
                                     </div>
                                 </div>
-                            </form>                            
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-        <div class="modal fade" id="cadastrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Cadastrar Evento</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <span id="msg-cad"></span>
-                        <form id="addevent" method="POST" enctype="multipart/form-data">
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Título</label>
-                                <div class="col-sm-10">
-                                    <input type="text" name="title" class="form-control" id="title" placeholder="Título do evento">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Color</label>
-                                <div class="col-sm-10">
-                                    <select name="color" class="form-control" id="color">
-                                        <option value="">Selecione</option>         
-                                        <option style="color:#FFD700;" value="#FFD700">Amarelo</option>
-                                        <option style="color:#0071c5;" value="#0071c5">Azul Turquesa</option>
-                                        <option style="color:#FF4500;" value="#FF4500">Laranja</option>
-                                        <option style="color:#8B4513;" value="#8B4513">Marrom</option>  
-                                        <option style="color:#1C1C1C;" value="#1C1C1C">Preto</option>
-                                        <option style="color:#436EEE;" value="#436EEE">Royal Blue</option>
-                                        <option style="color:#A020F0;" value="#A020F0">Roxo</option>
-                                        <option style="color:#40E0D0;" value="#40E0D0">Turquesa</option>
-                                        <option style="color:#228B22;" value="#228B22">Verde</option>
-                                        <option style="color:#8B0000;" value="#8B0000">Vermelho</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Início do evento</label>
-                                <div class="col-sm-10">
-                                    <input type="text" name="start" class="form-control" id="start" onkeypress="DataHora(event, this)">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Final do evento</label>
-                                <div class="col-sm-10">
-                                    <input type="text" name="end" class="form-control" id="end"  onkeypress="DataHora(event, this)">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-sm-10">
-                                    <button type="submit" name="CadEvent" id="CadEvent" value="CadEvent" class="btn btn-success">Cadastrar</button>                                    
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </body>
+</body>
 </html>
+
+
