21a324db7e86c33c51cd9996483b4aba1a3bb33e
DouglasStuhler <DouglasStuhler@github.com>
2020-07-29 23:08:08 -0300

    Atualização mostrar info model edit

--- agenda.php
+++ agenda.php
@@ -26,6 +26,12 @@ $resultado = mysqli_query($conexao_cad, $result_events);
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script>
 
+  var id = 0;
+  var titulo = "";
+  var dia = "";
+  var inicio = "";
+  var fim = "";
+
   document.addEventListener('DOMContentLoaded', function() {
 
     var calendarEl = document.getElementById('calendar');
@@ -50,11 +56,35 @@ $resultado = mysqli_query($conexao_cad, $result_events);
             // $('#cadastrar').dataToggle('modal');
             // $('#cadastrar').dataTarget('myModal');
         },
-      eventClick: function(arg) {
-        // if (confirm('Are you sure you want to delete this event?')) {
-        //   arg.event.remove()
-        // }
-        $('#cadastrar').modal('show');
+      eventClick: function(arg){
+
+        var selec = arg.event.startStr;
+        var selec_end = arg.event.endStr;
+
+        dia = selec.substring(0, selec.indexOf("T"));
+
+        inicio = selec.substring((selec.indexOf("T") + 1), (selec.indexOf("T") + 9));
+
+        if (fim == "" || fim == 0){
+
+          fim = "00:00:00";
+
+        } else {
+         
+          fim = selec_end.substring((selec_end.indexOf("T") + 1), (selec_end.indexOf("T") + 9));
+
+        }
+
+        id = arg.event.id;
+
+        titulo = arg.event.title;
+
+        $(".row #id").html(id);
+        $(".row #title").html(titulo);
+        $(".row #start").html(inicio);
+        $(".row #end").html(fim);
+
+        $('#visualizar').modal('show');
 
       },
       editable: true,
@@ -146,5 +176,86 @@ $resultado = mysqli_query($conexao_cad, $result_events);
                   </div>
                 </div>
               </div>
+
+              <div class="modal fade" id="visualizar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Detalhes do Evento</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="visevent">
+                            <dl class="row">
+                                <dt class="col-sm-3">ID do evento</dt>
+                                <dd class="col-sm-9" id="id"></dd>
+
+                                <dt class="col-sm-3">Título do evento</dt>
+                                <dd class="col-sm-9" id="title"></dd>
+
+                                <dt class="col-sm-3">Início do evento</dt>
+                                <dd class="col-sm-9" id="start"></dd>
+
+                                <dt class="col-sm-3">Fim do evento</dt>
+                                <dd class="col-sm-9" id="end"></dd>
+                            </dl>
+                            <button class="btn btn-warning btn-canc-vis">Editar</button>
+                            <a href="" id="apagar_evento" class="btn btn-danger">Apagar</a>
+                        </div>
+                        <div class="formedit" style="visibility: hidden;">
+                            <span id="msg-edit"></span>
+                            <form id="editevent" method="POST" enctype="multipart/form-data">
+                                <input type="hidden" name="id" id="id" >
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Título</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" name="title" class="form-control" id="title" placeholder="Título do evento">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Color</label>
+                                    <div class="col-sm-10">
+                                        <select name="color" class="form-control" id="color">
+                                            <option value="">Selecione</option>         
+                                            <option style="color:#FFD700;" value="#FFD700">Amarelo</option>
+                                            <option style="color:#0071c5;" value="#0071c5">Azul Turquesa</option>
+                                            <option style="color:#FF4500;" value="#FF4500">Laranja</option>
+                                            <option style="color:#8B4513;" value="#8B4513">Marrom</option>  
+                                            <option style="color:#1C1C1C;" value="#1C1C1C">Preto</option>
+                                            <option style="color:#436EEE;" value="#436EEE">Royal Blue</option>
+                                            <option style="color:#A020F0;" value="#A020F0">Roxo</option>
+                                            <option style="color:#40E0D0;" value="#40E0D0">Turquesa</option>
+                                            <option style="color:#228B22;" value="#228B22">Verde</option>
+                                            <option style="color:#8B0000;" value="#8B0000">Vermelho</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Início do evento</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" name="start" class="form-control" id="start" onkeypress="DataHora(event, this)">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Final do evento</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" name="end" class="form-control" id="end"  onkeypress="DataHora(event, this)">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-sm-10">
+                                        <button type="button" class="btn btn-primary btn-canc-edit">Cancelar</button>
+                                        <button type="submit" name="CadEvent" id="CadEvent" value="CadEvent" class="btn btn-warning">Salvar</button>                                    
+                                    </div>
+                                </div>
+                            </form>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </body>
 </html>
