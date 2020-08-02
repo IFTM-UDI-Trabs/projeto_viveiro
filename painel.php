292aaed831cb95a65dabcb894b6fc0fefd46306a
DouglasStuhler <DouglasStuhler@github.com>
2020-08-02 12:04:36 -0300

    CSS Painel

--- painel.php
+++ painel.php
@@ -2,7 +2,21 @@
 session_start();
 
 ?>
-
-<h2>Olá, <?php echo $_SESSION['usuario'];?></h2>
-<h2><a href="logout.php">Sair</a></h2>
-<h2><a href="agenda.php">Agenda</a></h2>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300;1,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/painel.css">
+    </head>
+    <body>
+        <div id="nav">
+            <img class="img" src="img/viveiro_logo.png" alt="Logo Viveiro">
+            <h2>Olá <?php echo $_SESSION['usuario'];?></h2>
+            <nav class="buttons">
+                <a href="logout.php">Sair</a>
+                <a href="agenda.php">Agenda</a>
+            </nav>
+        </div>
+    </body>
+</html>
